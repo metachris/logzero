bff29416764776cfd2a970db6dc9a45e948b4b8e
Chris Hager <chris.hager@bitsworking.com>
2017-07-04 15:12:24 +0200

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='logzero',
-    version='1.1.1',
+    version='1.1.2',
     description="Python logging made easy",
     long_description=readme + '\n\n' + history,
     author="Chris Hager",
