ae8cbf815ba717ebb921134a231d7ff4e929fa97
Chris Hager <chris@linuxuser.at>
2018-03-02 18:01:30 +0100

    Bump version: 1.3.1 → 1.4.0

--- logzero/__init__.py
+++ logzero/__init__.py
@@ -48,7 +48,7 @@ except ImportError:
 
 __author__ = """Chris Hager"""
 __email__ = 'chris@linuxuser.at'
-__version__ = '1.3.1'
+__version__ = '1.4.0'
 
 # Python 2+3 compatibility settings for logger
 bytes_type = bytes
